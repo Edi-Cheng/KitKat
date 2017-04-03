1d56aeefcffd5aea51465c783ac28377a20ffbe8
LynnMaple <lynn_maple@hotmail.com>
2017-04-03 12:23:28 +0800

    Init Project
    Create Project

---
+++ SavingsManagement/app/src/main/java/com/kitkat/savingsmanagement/AddSavingsItemActivity.java
@@ -0,0 +1,13 @@
+package com.kitkat.savingsmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSavingsItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_savings_item);
+    }
+}
