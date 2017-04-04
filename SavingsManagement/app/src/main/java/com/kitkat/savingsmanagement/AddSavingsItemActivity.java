e078ec0c133a72261ddd3f0f66657b6f8782a9b3
LynnMaple <lynn_maple@hotmail.com>
2017-04-04 22:29:05 +0800

    DatePicker
    User DatePicker instead of keyboard input.

--- SavingsManagement/app/src/main/java/com/kitkat/savingsmanagement/AddSavingsItemActivity.java
+++ SavingsManagement/app/src/main/java/com/kitkat/savingsmanagement/AddSavingsItemActivity.java
@@ -1,13 +1,74 @@
 package com.kitkat.savingsmanagement;
 
+import android.app.DatePickerDialog;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddSavingsItemActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddSavingsItemActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private String TAG = "Add Savings";
+
+    private EditText startDateEdit;
+    private EditText endDateEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_savings_item);
+
+        startDateEdit = (EditText)findViewById(R.id.edit_start_date);
+        endDateEdit = (EditText)findViewById(R.id.edit_end_date);
+
+        startDateEdit.setOnClickListener(showDatePicker);
+        endDateEdit.setOnClickListener(showDatePicker);
+
+        Button saveBtn = (Button) findViewById(R.id.btn_save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AddSavingsItemActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    private final View.OnClickListener showDatePicker = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Calendar c = Calendar.getInstance();
+            int startYear = c.get(Calendar.YEAR);
+            int startMonth = c.get(Calendar.MONTH);
+            int startDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    AddSavingsItemActivity.this,
+                    AddSavingsItemActivity.this,
+                    startYear, startMonth, startDay);
+
+            datePickerDialog.show();
+        }
+    };
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        String dateString = dayOfMonth + "-" + month + "-" + year;
+
+        Log.d(TAG, "onDateSet: "+ dateString);
+        
+        if (startDateEdit.isFocused()) {
+            startDateEdit.setText(dateString);
+        } else {
+            endDateEdit.setText(dateString);
+        }
     }
 }
