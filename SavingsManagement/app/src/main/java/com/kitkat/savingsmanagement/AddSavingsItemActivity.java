2886fcd5b023954d5b3450b82beb151ddbbbb467
LynnMaple <lynn_maple@hotmail.com>
2017-04-10 00:46:18 +0800

    Add the calculator method
    Add the calculator method

--- SavingsManagement/app/src/main/java/com/kitkat/savingsmanagement/AddSavingsItemActivity.java
+++ SavingsManagement/app/src/main/java/com/kitkat/savingsmanagement/AddSavingsItemActivity.java
@@ -12,13 +12,20 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class AddSavingsItemActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private String TAG = "Add Savings";
 
-    private EditText startDateEdit;
-    private EditText endDateEdit;
+    private EditText    startDateEdit;
+    private EditText    endDateEdit;
+    private EditText    amountEdit;
+    private EditText    annualizedEdit;
+    private EditText    expectedEdit;
+    private Date        startDate;
+    private Date        endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +34,60 @@ public class AddSavingsItemActivity extends AppCompatActivity implements DatePic
 
         startDateEdit = (EditText)findViewById(R.id.edit_start_date);
         endDateEdit = (EditText)findViewById(R.id.edit_end_date);
+        amountEdit = (EditText)findViewById(R.id.edit_amount);
+        annualizedEdit = (EditText)findViewById(R.id.edit_annualized_yield);
+        expectedEdit = (EditText)findViewById(R.id.edit_expected_interest);
 
 //        startDateEdit.setOnClickListener(showDatePicker);
 //        endDateEdit.setOnClickListener(showDatePicker);
         startDateEdit.setOnFocusChangeListener(focusChangeListener);
         endDateEdit.setOnFocusChangeListener(focusChangeListener);
 
+        annualizedEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+
+                    if (startDate == null) {
+                        startDateEdit.setFocusable(true);
+                        startDateEdit.requestFocus();
+                        return;
+                    }
+
+                    if (endDate == null || !endDate.after(startDate)) {
+                        endDateEdit.setFocusable(true);
+                        endDateEdit.requestFocus();
+                        return;
+                    }
+
+                    if (amountEdit.getText().toString().isEmpty()) {
+                        amountEdit.setFocusable(true);
+                        amountEdit.requestFocus();
+                        return;
+                    }
+
+                    double expexted = calculateExpectedInterest();
+                    expectedEdit.setFocusable(true);
+                    expectedEdit.requestFocus();
+                    expectedEdit.setText(String.valueOf(expexted));
+
+                }
+            }
+        });
+
         Button saveBtn = (Button) findViewById(R.id.btn_save);
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(AddSavingsItemActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button cancelBtn = (Button)findViewById(R.id.btn_cancel);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
@@ -76,16 +126,33 @@ public class AddSavingsItemActivity extends AppCompatActivity implements DatePic
         datePickerDialog.show();
     }
 
+    private double calculateExpectedInterest() {
+
+        // amount * (enddate - startdate) / 365 * annualized_yield = expected interest
+
+        double amount = Double.parseDouble(amountEdit.getText().toString());
+        double annulized = Double.parseDouble(annualizedEdit.getText().toString());
+
+        long diff = endDate.getTime() - startDate.getTime();
+        long days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+
+        return  amount * days / 365 * annulized;
+    }
+
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-        String dateString = dayOfMonth + "-" + month + "-" + year;
+        String dateString = dayOfMonth + "-" + (month+1) + "-" + year;
 
         Log.d(TAG, "onDateSet: "+ dateString);
-        
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month+1, dayOfMonth);
         if (startDateEdit.isFocused()) {
+            startDate = calendar.getTime();
             startDateEdit.setText(dateString);
         } else {
+            endDate = calendar.getTime();
             endDateEdit.setText(dateString);
         }
     }
